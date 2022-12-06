37779a67f25999479d4cf2c88471098227e9857c
saulius334 <105633620+saulius334@users.noreply.github.com>
2022-12-06 13:51:55 +0200

    Seeder upgrade

--- database/seeders/RefugeeSeeder.php
+++ database/seeders/RefugeeSeeder.php
@@ -12,7 +12,10 @@ class RefugeeSeeder extends Seeder
     {
         $refugeeCount = 30;
         $faker = Faker::create();
-
+        $pets = [null, 'Cat', 'Dog', 'Bebr', 'kot', null];
+        $destinations = [null, 'Lituania', 'Kyiv', 'Kharkiv', 'Odesa', 'Dnipro', 'Donetsk', 'Poland', null];
+        $yesno = [null, 'Yes', 'No'];
+        $feeling = [null, 'Bad', 'Normal', 'Good'];
         foreach (range(0,$refugeeCount) as $_) {
             DB::table('refugees')->insert([
                 'name' => $faker->firstName(),
@@ -21,23 +24,13 @@ class RefugeeSeeder extends Seeder
                 'bedsTaken' => $faker->numberBetween(1,5),
                 'current_refugee_camp_id' => $faker->numberBetween(1,2),
                 'confirmed' => $faker->boolean(50),
+                'pets' => $pets[rand(0,5)],
+                'aidReceived' => $yesno[rand(0,2)],
+                'healthCondition' => $feeling[rand(0,3)],
+                'destination' => $destinations[rand(0,8)],
+                'created_at' => $faker->dateTimeBetween('-2 months'),
+                'updated_at' => $faker->dateTimeThisMonth(),
             ]);
         }
-        // DB::table('refugees')->insert([
-        //     'name' => 'Kibiras',
-        //     'surname' => 'BMW',
-        //     'IdNumber' => '0955554321',
-        //     'bedsTaken' => '5',
-        //     'current_refugee_camp_id' => '1',
-        //     'confirmed' => true
-        // ]);
-        // DB::table('refugees')->insert([
-        //     'name' => 'Kibiras',
-        //     'surname' => 'BMW',
-        //     'IdNumber' => '0955554321',
-        //     'bedsTaken' => '5',
-        //     'current_refugee_camp_id' => '1',
-        //     'confirmed' => true
-        // ]);
     }
 }
