255ed2ab477d53a0a42a74786fa9470d74619801
saulius334 <105633620+saulius334@users.noreply.github.com>
2022-12-08 10:14:32 +0200

    fixed IdNumber

--- database/seeders/RefugeeSeeder.php
+++ database/seeders/RefugeeSeeder.php
@@ -20,7 +20,7 @@ class RefugeeSeeder extends Seeder
             DB::table('refugees')->insert([
                 'name' => $faker->firstName(),
                 'surname' => $faker->lastName(),
-                'IdNumber' => $faker->numberBetween(10,10),
+                'IdNumber' => $faker->numerify('##########'),
                 'bedsTaken' => $faker->numberBetween(1,5),
                 'current_refugee_camp_id' => $faker->numberBetween(1,2),
                 'confirmed' => $faker->boolean(50),
