57faf12587293bf317eac6d060e649f2d53501ad
saulius334 <105633620+saulius334@users.noreply.github.com>
2022-12-14 11:16:22 +0200

    cleanup

--- app/Services/User/UserRefugeesService.php
+++ app/Services/User/UserRefugeesService.php
@@ -4,8 +4,9 @@ declare(strict_types=1);
 
 namespace App\Services\User;
 
-use App\Repositories\Interfaces\RefugeeCampRepositoryInterface;
+use Illuminate\Database\Eloquent\Collection;
 use App\Repositories\Interfaces\RefugeeRepositoryInterface;
+use App\Repositories\Interfaces\RefugeeCampRepositoryInterface;
 
 class UserRefugeesService
 {
@@ -13,18 +14,16 @@ class UserRefugeesService
     public function __construct(
         private RefugeeRepositoryInterface $refugeeRepo,
         private RefugeeCampRepositoryInterface $campRepo,
-        )
-    {
+    ) {
     }
 
-    public function getRefugeesByUserId(int $userId, int $confirmed)
+    public function getRefugeesByUserId(int $userId, int $confirmed): ?Collection
     {
         $campList = $this->campRepo->getCampsByUserId($userId);
-        $refugees = [];
         foreach ($campList as $camp) {
-            $refugees[] = $this->refugeeRepo->getRefugeesByCampId($camp->id);
+            $refugees = $this->refugeeRepo->getRefugeesByCampId($camp->id);
         }
-        $result = $refugees[0]->filter(function($value) use ($confirmed) {
+        $result = $refugees->filter(function ($value) use ($confirmed) {
             return $value->confirmed == $confirmed;
         });
         return $result;
