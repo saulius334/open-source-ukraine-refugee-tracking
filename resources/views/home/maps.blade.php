64e562f7327a7d6b9519fdd51e935c58e9f059ed
saulius334 <105633620+saulius334@users.noreply.github.com>
2022-12-19 10:25:25 +0200

    maps blade template

---
+++ resources/views/home/maps.blade.php
@@ -0,0 +1,17 @@
+@extends('components.layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Ukraine Refugee Tracking Map</div>
+
+                <div class="card-body">
+                    <p>Here you can see all the registered refugee camps. Please click on one and select to register.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
